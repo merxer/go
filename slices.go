7125af5b3c8a054eed513fc4d47dd3931644f002
Thawatchai Singngam <merxer.io@gmail.com>
2018-12-15 16:08:06 +0700

    slice operator

--- slices.go
+++ slices.go
@@ -5,5 +5,8 @@ import "fmt"
 func main() {
 	s := []int {1,2,3}
 
-	fmt.Printf("%v\n", s)
+	fmt.Printf("%v\n", s[1:2])
+	fmt.Printf("%v\n", s[1:])
+	fmt.Printf("%v\n", s[2:])
+	fmt.Printf("%v\n", s[:3])
 }
