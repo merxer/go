6b7fd45beb84d04db04bf9c29851adba19708a3e
Thawatchai Singngam <merxer.io@gmail.com>
2018-12-15 13:23:48 +0700

    implicit types

--- variables.go
+++ variables.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var message string = "Hello Go World"
-	var a, b, c int = 1, 2, 3
+	var message = "Hello Go World"
+	var a, b, c = 1, 2, 3
 
 
 	fmt.Println(message, a, b, c)
