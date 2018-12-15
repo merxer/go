23030507ad2f9ce8eb1af630e923e1825892701d
Thawatchai Singngam <merxer.io@gmail.com>
2018-12-15 18:52:25 +0700

    run and sleep

--- concurrency.go
+++ concurrency.go
@@ -5,14 +5,17 @@ import (
 )
 
 func main() {
+	godur, _ := time.ParseDuration("10ms")
 	go func() {
 		for i := 0; i < 100; i++ {
 			println(i, "Hello")
+			time.Sleep(godur)
 		}
 	}()
 	go func() {
 		for i := 0; i < 100; i++ {
 			println(i, "Go")
+			time.Sleep(godur)
 		}
 	}()
 
